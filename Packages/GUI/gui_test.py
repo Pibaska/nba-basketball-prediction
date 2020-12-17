4dcab1d8c2377fc4126c871da0f50fe076cd1160
Bernardo Mesko <bemesko@gmail.com>
2020-12-17 20:07:12 -0300

    Base do Layout Programada

--- Packages/GUI/gui_test.py
+++ Packages/GUI/gui_test.py
@@ -1,30 +1,66 @@
-"""
-Interface Simples de um tutorial que eu vi por aí
-link: https://realpython.com/python-pyqt-gui-calculator/
-
-Instruções pra rodar isso aqui:
-1. Cria um ambiente virtual digitando "python -m venv venv" no terminal
-2. Instala o PyQt5 com "pip install -r requirements.txt"
-3. Só
-
-~Bernardo
-"""
-
 import sys
 
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QApplication, QLineEdit, QMainWindow
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QLabel
+from PyQt5 import QtCore
 
-app = QApplication(sys.argv)
 
-window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
+class BasketballPredictionView(QMainWindow):
+    """
+    Protótipo da interface gráfica para o programa
+    """
 
-window.show()
+    def __init__(self):
+        super().__init__()
 
-sys.exit(app.exec_())
+        self.setWindowTitle('Basketball Prediction - Protótipo')
+
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+
+        self._centralWidget.setLayout(self.setup_layout())
+
+    def setup_layout(self):
+        """ Faz as configurações necessárias do layout manager"""
+        core_layout = QVBoxLayout()
+        core_layout.addWidget(self.setup_title_label())
+
+        return core_layout
+
+    def setup_title_label(self):
+        title_label = QLabel("BASKETBALL PREDICTION")
+        title_label.setAlignment(QtCore.Qt.AlignCenter)
+
+        return title_label
+
+
+def main():
+    basketballGUI = QApplication(sys.argv)
+
+    view = BasketballPredictionView()
+    view.show()
+
+    sys.exit(basketballGUI.exec())
+
+
+if __name__ == "__main__":
+    main()
+
+
+# app = QApplication(sys.argv)
+# window = QWidget()
+
+
+# main_layout = QVBoxLayout()
+
+# main_layout.addWidget(QLabel("BASKETBALL PREDICTION"))
+# main_layout.addWidget(QLabel('Layout dos combobox'))
+# main_layout.addWidget(QPushButton('Prever'))
+# main_layout.addWidget(QLineEdit("Resultado:"))
+
+# window.setLayout(main_layout)
+# window.show()
+# sys.exit(app.exec_())
