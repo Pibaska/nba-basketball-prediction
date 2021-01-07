92aba7367e7e38c375ca571836454f0950c613b2
Grégori Sabel <seriosabel57@gmail.com>
2021-01-07 11:23:06 -0300

    criador de url feito

--- Packages/Web-Scraping/por_partidas.py
+++ Packages/Web-Scraping/por_partidas.py
@@ -1,45 +1,81 @@
+
+# esse codigo deve: -------------------------------------------------------
+# função que repete tudo a baixo por todos os dias de uma temporada
+# entra na tela de Jogos de um dia expecífico
+# ve quantos jogos tem
+# entra no "Box Score" do primeiro jogo pega as coisas, volta, e entra no proximo
+# salva em json
+# -------------------------------------------------------------------------
+
 import time
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import json
-
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
-option = Options()
-option.headless = True
-driver = webdriver.Firefox(firefox_binary=binary,executable_path=r'C:\\geckodriver.exe')
+def SetupDriver():
+    binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+    option = Options()
+    option.headless = True
+    driver = webdriver.Firefox(firefox_binary=binary,executable_path=r'C:\\geckodriver.exe')
+    return driver
 
-
-
-url = "https://www.basketball-reference.com/boxscores/?month=5&day=3&year=2019"
-
+url = "https://www.basketball-reference.com/boxscores/?month=5&day=4&year=2020" #url da tela de partidas
 xPathTabela = ""    
-
-nomeArquivo =  "partidas" 
-nomeDicionario = "partidas_2019-05-"
+nomeArquivo =  "partidas" #arquivo .json
+nomeDicionarioAno = "partidas_x" #nome que vai se alterar por dia  
+nomeDicionarioDia = "partidas_x/x/x" #nome que vai se alterar por dia  
 dicionario = {}
+diasNosMeses = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
+#Retorna quantas partidas o site tem do dia escolhido
+def NeoContadorDePartidas():
+    element = driver.find_element_by_xpath('//*[@id="content"]/div[3]') # puxa a div que tem os jogos
+    html_content = element.get_attribute('outerHTML') # pega seu HTML
+    soup = BeautifulSoup(html_content, 'html.parser') # Transforma em algo facil de mexer
 
-#Descobre quantas partidas o site tem do dia escolhido
-def neoContadorDePartidas():
-    element = driver.find_element_by_xpath('//*[@id="content"]/div[3]')
-    html_content = element.get_attribute('outerHTML')
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    qtdPartidas = soup.find_all("div", {"class": "game_summary expanded nohover"}) # pega só divs que tenham tal classe
-    qtd = len(qtdPartidas)
+    Partidas = soup.find_all("div", {"class": "game_summary expanded nohover"}) # pega só divs que tenham tal classe
+    qtd = len(Partidas) # Vê quantas achou
 
     return qtd
 
+def FazAsCoisas(dia, mes, ano):
+    #aqui é onde ce vai fazer os processo pesado. (no fim é onde tu vai chamar um monte de função)
+    dia += 1
+    mes += 1
+    ano += 2000
+    # cria e chama a url
+    url = f"https://www.basketball-reference.com/boxscores/?month={mes}&day={dia}&year=2020"
 
-driver.get(url)
+    print(f'{dia}/{mes}/{ano }')
 
-qtdPartidas = neoContadorDePartidas()
 
-print('No dia escolhido ocorreram: ' + str(qtdPartidas) + ' partidas')
+#------------------------------------------------------------------------------ as coisa
 
+for ano in range(20):
+    # calcula se é ano bissexto
+    trueAno = 2000 + ano
+    bissexto = 1 if (trueAno % 4 == 0 and (trueAno % 400 == 0 or trueAno % 100 != 0)) else 0 # opeador ternário
+
+    for mes in range(12):
+        rangeDias = diasNosMeses[mes]
+        rangeDias += bissexto if (rangeDias == 28) else 0
+
+        for dia in range (rangeDias):
+            FazAsCoisas(dia, mes, ano)
+            
+
+#driver = SetupDriver()
+
+#driver.get(url)
+
+#qtdPartidas = NeoContadorDePartidas()
+
+#print('No dia escolhido ocorreram: ' + str(qtdPartidas) + ' partidas')
+
+
+#driver.quit()
