7a560fb1bf6052e22e404a232e862ec904b6de3e
Bernardo Mesko <bemesko@gmail.com>
2021-01-08 12:48:15 -0300

    Cria estrutura para os dados de entrada da função fitness

---
+++ Packages/Utils/genetic_alg_fake_data.py
@@ -0,0 +1,28 @@
+match_database = {
+    "match01": {
+        "home_team_data": {
+            "average_1q_score": 5,
+            "1q_home_data": {
+                "ratio": 1.5,
+                "spread": 2
+            },
+            # talvez esse away não precise estar aqui
+            "1q_away_data": {
+                "ratio": 0.8,
+                "spread": 4
+            }
+        },
+        "away_team_data": {
+            "average_1q_score": 5,
+            "1q_home_data": {
+                "ratio": 1.5,
+                "spread": 2
+            },
+            "1q_away_data": {
+                "ratio": 0.8,
+                "spread": 4
+            }
+        },
+        "home_team_win_1q": True
+    }
+}
