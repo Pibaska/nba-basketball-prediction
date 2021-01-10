ee27c31dcd1f677599471c0f428fd8942d4527a8
Bernardo Mesko <bemesko@gmail.com>
2021-01-10 17:00:03 -0300

    Muda formato dos dicionários nos dados de teste para serem iteráveis

--- Packages/Utils/genetic_alg_fake_data.py
+++ Packages/Utils/genetic_alg_fake_data.py
@@ -1,35 +1,28 @@
 match_database = {
     "match01": {
-        "home_team_data": {
+        "home_team_stats": {
             "average_1q_score": 5,
-            "1q_home_stats": {
-                "ratio": 1.5,
-                "spread": 2
-            },
+            "1q_home_ratio": 1.5,
+            "1q_home_spread": 2,
+
             # talvez esse away não precise estar aqui
-            "1q_away_stats": {
-                "ratio": 0.8,
-                "spread": 4
-            },
-            "1q_last10games_stats": {
-                "ratio": 2,
-                "spread": 1.7
-            }
+            "1q_away_ratio": 0.8,
+            "1q_away_spread": 4,
+
+            "1q_last10games_ratio": 2,
+            "1q_last10games_spread": 1.7
         },
-        "away_team_data": {
+        "away_team_stats": {
             "average_1q_score": 5,
-            "1q_home_stats": {
-                "ratio": 1.5,
-                "spread": 2
-            },
-            "1q_away_stats": {
-                "ratio": 0.8,
-                "spread": 4
-            },
-            "1q_last10games_stats": {
-                "ratio": 2,
-                "spread": 1.7
-            }
+
+            # Esse home também não precisaria estar aqui
+            "1q_home_ratio": 1.5,
+            "1q_home_spread": 2,
+
+            "1q_away_ratio": 0.8,
+            "1q_away_spread": 4,
+            "1q_last10games_ratio": 2,
+            "1q_last10games_spread": 1.7
         },
         "home_team_won_1q": True
     }
