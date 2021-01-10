afd06a872c0594b0a6fff992deb9308c6fdb7eb4
Bernardo Mesko <bemesko@gmail.com>
2021-01-10 16:43:58 -0300

    Acrescenta dados das últimas 10 partidas no dict de teste

--- Packages/Utils/genetic_alg_fake_data.py
+++ Packages/Utils/genetic_alg_fake_data.py
@@ -2,27 +2,35 @@ match_database = {
     "match01": {
         "home_team_data": {
             "average_1q_score": 5,
-            "1q_home_data": {
+            "1q_home_stats": {
                 "ratio": 1.5,
                 "spread": 2
             },
             # talvez esse away não precise estar aqui
-            "1q_away_data": {
+            "1q_away_stats": {
                 "ratio": 0.8,
                 "spread": 4
+            },
+            "1q_last10games_stats": {
+                "ratio": 2,
+                "spread": 1.7
             }
         },
         "away_team_data": {
             "average_1q_score": 5,
-            "1q_home_data": {
+            "1q_home_stats": {
                 "ratio": 1.5,
                 "spread": 2
             },
-            "1q_away_data": {
+            "1q_away_stats": {
                 "ratio": 0.8,
                 "spread": 4
+            },
+            "1q_last10games_stats": {
+                "ratio": 2,
+                "spread": 1.7
             }
         },
-        "home_team_win_1q": True
+        "home_team_won_1q": True
     }
 }
