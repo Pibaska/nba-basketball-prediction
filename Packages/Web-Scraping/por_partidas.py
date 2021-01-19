f5f556046656562548fb1a89f540f100e8ed9dce
greg <seriosabel57@gmail.com>
2021-01-19 10:11:41 -0300

    modularização

--- Packages/Web-Scraping/por_partidas.py
+++ Packages/Web-Scraping/por_partidas.py
@@ -15,12 +15,12 @@ import json
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
+
 # url = "https://www.basketball-reference.com/boxscores/?month=5&day=4&year=2020" #url da tela de partidas
 # nomeArquivo =  "partidas" #arquivo .json
 # nomeDicionarioAno = "partidas_x" #nome que vai se alterar por dia
 # nomeDicionarioDia = "partidas_x/x/x" #nome que vai se alterar por dia
-dicionario = {}
-diasNosMeses = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 
 
 def SetupDriver(mostra):
@@ -33,7 +33,7 @@ def SetupDriver(mostra):
 
 
 # Retorna quantas partidas o site tem do dia escolhido
-def ContadorDePartidas():
+def ContadorDePartidas(driver):
     element = driver.find_element_by_xpath(
         '//*[@id="content"]/div[3]')  # puxa a div que tem os jogos
     html_content = element.get_attribute('outerHTML')  # pega seu HTML
@@ -46,7 +46,7 @@ def ContadorDePartidas():
 
     return qtd
 
-
+#função de teste
 def TestaBissexto():
     for ano in range(2000):
         # calcula se é ano bissexto
@@ -54,22 +54,21 @@ def TestaBissexto():
         if (trueAno % 4 == 0 and (trueAno % 400 == 0 or trueAno % 100 != 0)):
             print(trueAno)
 
-
+#pega os valores das tabelas
 def PegaComponente(tabela,  coletavel, nomeColetavel):
     componente = tabela.find_all("td", {"data-stat": coletavel})[0]
     valorColetado = componente.get_text()
     print(f'{nomeColetavel}: {valorColetado}')
 
-
-def FazColeta():
-    # pela class [0] [2] class="sortable stats_table now_sortable"
+#coleta nome dos times e pega as trabelas
+def FazColeta(driver):
     tudo = '//*[@id="content"]'
     elementDeTudo = driver.find_element_by_xpath(tudo)
     html_contentDeTudo = elementDeTudo.get_attribute('outerHTML')
     sopaDeTudo = BeautifulSoup(html_contentDeTudo, 'html.parser')
 
     tabelasTodas = sopaDeTudo.find_all(
-        "table", {"class": "sortable stats_table now_sortable"})
+        "table", {"class": "sortable stats_table now_sortable"}) 
     tabelasDosTimes = [tabelasTodas[0], tabelasTodas[2]]
 
     caixaTimesNomes = sopaDeTudo.find("div", {"class": "scorebox"})
@@ -81,7 +80,7 @@ def FazColeta():
                             'De 3  ']
     print('--partida--')
     for x in range(2):
-        local = 'casa' if x else 'fora'
+        local = 'casa' if not x else 'fora'
         nome = nomes[x].get_text()
 
         print(f'{local} - {nome}')
@@ -92,18 +91,18 @@ def FazColeta():
 
         print('-')
 
-
-def PuxaJogosDoDia(dia, mes, ano):
+#recebendo os dias, navega pelo site e entra nos jogos
+def PuxaJogosDoDia(driver, dia, mes, ano):
     # aqui é onde ce vai fazer os processo pesado. (no fim é onde tu vai chamar um monte de função)
     dia += 1
     mes += 1
     # cria e chama a url
-    url = f"https://www.basketball-reference.com/boxscores/?month={mes}&day={dia}&year={ano}"
+    url = f"https://www.basketball-reference.com/boxscores/?month={mes}&day={dia}&year={ano}"   
     driver.get(url)
     print('---------D-I-A---------')
     print(f'{dia}/{mes}/{ano }')
 
-    qtdJogos = ContadorDePartidas()
+    qtdJogos = ContadorDePartidas(driver)
     print(f'Partidas: {qtdJogos}')
 
     for i in range(qtdJogos):  # entra nos jogos para ver mais detalher
@@ -111,11 +110,12 @@ def PuxaJogosDoDia(dia, mes, ano):
         # entra no box-score do jogo
         driver.find_element_by_xpath(
             f'//*[@id="content"]/div[3]/div[{i+1}]/p/a[1]').click()
+
         # Para apresentar apenas 1°quarto
         driver.find_element_by_xpath(
             f'//*[@id="content"]/div[6]/div[2]/a').click()
 
-        FazColeta()
+        FazColeta(driver)
         # coletar da tabela
         # mandar pra um dicionário de dicionarios provavelmente
 
@@ -123,20 +123,27 @@ def PuxaJogosDoDia(dia, mes, ano):
 # ------------------------------------------------------------------------------ as coisa
 # escolhe False mostra o firefox sendo aberto. True faz escondido
 # se deixar escondido lembre de checar pelo gerenciador de tarefas
-driver = SetupDriver(False)
 
-for ano in range(20):
-    # calcula se é ano bissexto
-    trueAno = 2000 + ano
-    bissexto = 1 if (trueAno % 4 == 0 and (trueAno %
-                                           400 == 0 or trueAno % 100 != 0)) else 0  # opeador ternário
+# driver = SetupDriver(False)
 
-    for mes in range(12):
-        rangeDias = diasNosMeses[mes]
-        rangeDias += bissexto if (rangeDias == 28) else 0
+# for ano in range(20):
+#     # calcula se é ano bissexto
+#     trueAno = 2000 + ano
+#     bissexto = 1 if (trueAno % 4 == 0 and (trueAno %
+#                                            400 == 0 or trueAno % 100 != 0)) else 0  # opeador ternário
 
-        for dia in range(rangeDias):
-            PuxaJogosDoDia(dia, mes, trueAno)
+#     for mes in range(12):
+#         rangeDias = diasNosMeses[mes]
+
+#         rangeDias += bissexto if (rangeDias == 28) else 0
+
+#         for dia in range(rangeDias):
+#             PuxaJogosDoDia(dia, mes, trueAno)
+
+print(20*'~~')
+print(20*'~~')
+print("modulo errado, da play no 'main.py'")
+print(20*'~~')
+print(20*'~~')
 
 
-driver.quit()
