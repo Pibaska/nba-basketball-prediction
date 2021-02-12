d4c5f3d70a476ff5bd440f7b85d38abe8660ea4d
Bernardo Mesko <bemesko@gmail.com>
2021-02-12 20:44:20 -0300

    Overhaul nas funções de banco de dados

--- src/utils/database/database_definition.py
+++ src/utils/database/database_definition.py
@@ -13,42 +13,52 @@ Output:
     -------
     '''
     cursor.executescript("""
-    CREATE TABLE team_participation(
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS team_participation (
+        id INTEGER NOT NULL PRIMARY KEY,
         team_name VARCHAR(50),
-        minutes_played INTEGER,
-        field_goals INTEGER,
-        field_goals_attempts INTEGER,
-        field_goals_percentage DECIMAL(4,3),
-        par_3point_field_goals INTEGER,
-        par_3point_field_goals_attempts INTEGER,
-        par_3point_field_goals_percentage DECIMAL(4,3),
-        free_throws INTEGER,
-        free_throws_attempts INTEGER,
-        free_throws_percentage DECIMAL(4,3),
-        offensive_rebounds INTEGER,
-        defensive_rebounds INTEGER,
-        total_rebounds INTEGER,
-        assists INTEGER,
-        steals INTEGER,
-        blocks INTEGER,
-        turnover INTEGER,
-        personal_faults INTEGER,
-        points INTEGER,
-        mat_count_by_team INTEGER,
-        won BIT,
-        team_is_home BIT
-    );              
-    CREATE TABLE match(
-        mat_ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        team_location VARCHAR(50),
+        minutes_played VARCHAR(10) NOT NULL,
+        field_goals INTEGER NOT NULL,
+        field_goals_attempts INTEGER NOT NULL,
+        field_goals_percentage DECIMAL(4,3) NOT NULL,
+        three_point_field_goals INTEGER NOT NULL,
+        three_point_field_goals_attempts INTEGER NOT NULL,
+        three_point_field_goals_percentage DECIMAL(4,3) NOT NULL,
+        free_throws INTEGER NOT NULL,
+        free_throws_attempts INTEGER NOT NULL,
+        free_throws_percentage DECIMAL(4,3) NOT NULL,
+        offensive_rebounds INTEGER NOT NULL,
+        defensive_rebounds INTEGER NOT NULL,
+        total_rebounds INTEGER NOT NULL,
+        assists INTEGER NOT NULL,
+        steals INTEGER NOT NULL,
+        blocks INTEGER NOT NULL,
+        turnover INTEGER NOT NULL,
+        personal_faults INTEGER NOT NULL,
+        points INTEGER NOT NULL,
+        mat_count_by_team INTEGER NOT NULL,
+        won BIT NOT NULL,
+        team_is_home BIT NOT NULL,
+        fk_match INTEGER NOT NULL,
+        FOREIGN KEY (fk_match) REFERENCES match_data (id)
+    );
+    CREATE TABLE IF NOT EXISTS match_data(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         fk_team_home INTEGER NOT NULL,
         fk_team_away INTEGER NOT NULL,
         date DATE NOT NULL,
         count INTEGER NOT NULL,
-        FOREIGN KEY (fk_team_home) references team_participation(id),
-        FOREIGN KEY (fk_team_away) references team_participation(id)
+        FOREIGN KEY (fk_team_home) references team_participation (id),
+        FOREIGN KEY (fk_team_away) references team_participation (id)
     );""")
     print('Tabela criada com sucesso.')
+
+
+def drop_tables(cursor):
+    cursor.executescript("""
+        DROP TABLE match_data;
+        DROP TABLE team_participation;
+    """)
 
 
 if __name__ == "__main__":
@@ -56,4 +66,10 @@ if __name__ == "__main__":
     db = sqlite3.connect('data/database.sqlite3')
     cursor = db.cursor()
 
-    create_database(cursor)
+    try:
+        create_database(cursor)
+    except Exception as exception:
+        print(exception)
+        raise(exception)
+    finally:
+        db.close()
--- src/utils/database/database_manipulation.py
+++ src/utils/database/database_manipulation.py
@@ -1,18 +1,50 @@
 import sqlite3
 
 
-def insert_team_participation_data(cursor, data):
-    cursor.executemany("INSERT INTO banana VALUES (?, ?)", data)
+def insert_team_participation_data(cursor, team_data):
+    cursor.executemany("""
+    INSERT INTO team_participation (
+        team_name,
+        team_location,
+        minutes_played,
+        field_goals,
+        field_goals_attempts,
+        field_goals_percentage,
+        three_point_field_goals,
+        three_point_field_goals_attempts,
+        three_point_field_goals_percentage,
+        free_throws,
+        free_throws_attempts,
+        free_throws_percentage,
+        offensive_rebounds,
+        defensive_rebounds,
+        total_rebounds,
+        assists,
+        steals,
+        blocks,
+        turnover,
+        personal_faults,
+        points,
+        mat_count_by_team,
+        won,
+        team_is_home,
+        fk_match) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""", team_data)
 
 
-def retrieve_team_participation_data(cursor):
-    cursor.execute("""SELECT * FROM team_participation""")
+def retrieve_team_participation_data(cursor, fk_match, team_is_home):
+    return cursor.execute(
+        """SELECT * FROM team_participation WHERE fk_match = ? AND team_is_home = ?""", (fk_match, team_is_home))
 
-    rows = cursor.fetchall()
-    return rows
 
-def retrieve_match_data(cursor):
-    cursor.execute("""SELECT * FROM match""")
+def retrieve_match_data(cursor, match_id):
+    return cursor.execute("""SELECT * FROM match WHERE id = ?""", match_id)
+
+
+def check_tables(cursor):
+    cursor.execute(""" SELECT * FROM team_participation;""")
+    print(cursor.fetchall())
+    cursor.execute("""SELECT * FROM match_data""")
+    print(cursor.fetchall())
 
 
 if __name__ == "__main__":
@@ -20,20 +52,14 @@ if __name__ == "__main__":
     db = sqlite3.connect('data/database.sqlite3')
     cursor = db.cursor()
 
-    fake_team_data = [(2, "def"), (3, "ghi"), (4, "jkl")]
+    fake_team_data = [('Magic', 'Orlando 2', '39:56', 7, 20, 0.350, 0,
+                       0, 0.350, 8, 8, 1.000, 4, 5, 9, 1, 1, 0, 5, 4, 22, 3, 0, 1, 1)]
 
     try:
-        cursor.execute("""CREATE TABLE if not exists banana(
-        numero INTEGER,
-        string VARCHAR(4)
-        );""")
-
         insert_team_participation_data(cursor, fake_team_data)
-        print(retrieve_team_participation_data(cursor))
-        pass
+        check_tables(cursor)
     except Exception as e:
         print(e)
         raise e
     finally:
-        cursor.execute("DROP TABLE banana")
         db.close()
