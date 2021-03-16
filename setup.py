9d4ce82c6ad142680e271cc405815ef8e01edeb0
João Vitor Waldrich <jv.waldrich0@protonmail.com>
2021-03-16 17:10:58 -0300

    Setup V0.5

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from os import system, name
 
 
 if name == 'nt':
-    system('python -m venv config/env')
-    system('config/env/Scripts/activate.bat')
+    system('python -m venv .')
+    system('./Scripts/activate.bat')
     try:
-        system('pip install -r config/requirements.txt')
+        system('pip install -r requirements.txt')
     except Exception:
-        system('pip install -r config/requirements.txt --no-dependencies')
+        system('pip install -r requirements.txt --no-dependencies')
     finally:
         pass
 else:
     system('''
-           python -m venv config/env || python3 -m venv config/env;
-           source config/env/bin/activate;
-           pip install -r config/requirements.txt || pip install -r config/requirements.txt --no-dependencies''')
+           python -m venv . || python3 -m venv .;
+           source ./bin/activate;
+           pip install -r requirements.txt || pip install -r config/requirements.txt --no-dependencies''')
