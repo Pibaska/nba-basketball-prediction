8f7c480fff2efadfa332a0ecd895e941992f3ac7
João Vitor Waldrich <jv.waldrich0@protonmail.com>
2021-03-16 14:37:04 -0300

    removendo comentarios desnecessarios

--- src/main.py
+++ src/main.py
@@ -9,23 +9,10 @@ from gui.model import run_gen_alg
 from web_scraping.main import activate_web_scraping
 from datetime import datetime as dt
 
-
-# basketballGUI = QApplication(sys.argv)
-
-# view = BasketballPredictionView()
-# view.show()
-# basketballGUI.setStyleSheet(view.stylesheet)
-
-# BasketballPredictionController(view)
-
-# sys.exit(basketballGUI.exec())
-
 print("""
     [1] Algoritmo Genético (vai treinar populações com os dados disponiveis no BD)
     [2] Web Scraping (vai coletar dados a partir do dia em que parou, se houver partidas. E salvar no BD)
 """)
-
-
 
 x = input("Queres rodar o que?")
 
