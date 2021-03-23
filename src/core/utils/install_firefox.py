5deecf3ad1eaccabbb393811a17ac02391da9397
João Vitor Waldrich <jv.waldrich0@protonmail.com>
2021-03-23 18:27:50 +0000

    bug fix firefox installer

--- src/core/utils/install_firefox.py
+++ src/core/utils/install_firefox.py
@@ -6,4 +6,4 @@ from os.path import join
 def install_firefox(choice=True):
     if choice:
         system(join(Directory(Path(__file__).resolve().parent.parent.parent).cwd,
-        'library', 'Firefox Setup 87.0.exe') if name == 'nt' else system('sudo pacman -Syy firefox || sudo apt-get install firefox || sudo emerge firefox || rpm install firefox')
+        'library', 'Firefox Setup 87.0.exe') if name == 'nt' else system('sudo pacman -Syy firefox || sudo apt-get install firefox || sudo emerge firefox || sudo rpm install firefox'))
