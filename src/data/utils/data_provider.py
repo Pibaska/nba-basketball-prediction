84a0b14c1635aa2fcee7960ef2ae9444b2858a34
Grégori Sabel <seriosabel57@gmail.com>
2021-03-31 11:28:55 -0300

    ao coletar as partidas de uma season, irá ignorar os 10 primeiros dias

--- src/data/utils/data_provider.py
+++ src/data/utils/data_provider.py
@@ -17,6 +17,7 @@ import random
 from pathlib import Path
 from os.path import join
 from datetime import datetime as dt
+from datetime import timedelta  
 from core.utils.directory_manipulation import Directory
 
 # dicionário com começo e final das seasons, de 2000 até 2020
@@ -46,48 +47,6 @@ seasons = {
     "2020": {"start": "2020-12-22", "end": "2021-05-16"},
     "2021": {"start": "2021-10-31", "end": "2022-04-10"}
 }
-
-# o AG vai fazer o fit com partidas começadas apos 10 dias do começo da temporada
-# e então o AG vai se movimentar para frente no tempo apenas, e usará uma quantidade de jogos
-#  Poderiamos previamente selecionar jogos para o fit
-# É uma questão importante pois o fit funcionaria com um jogo só de DATA
-# todas funções aqui vão rodar dentro de um dia
-
-
-# todos jogos do Atlanta Hawks(id=2) em casa a partir do dia '08/01/2000' ate o dia '20/02/2000'
-""" 
-SELECT * from match_data as md INNER JOIN participation as pt 
-    ON md.fk_participation_home = pt.participation_id 
-    WHERE pt.fk_team_id = 2 
-    and pt.team_is_home = 1
-    and md.date > '2000-01-08'
-    and md.date < '2000-02-20'
-    order by md.date ASC;
-"""
-# MEDIA da coisa de cima
-"""
-SELECT 
-  AVG(pt.points), --Pontos h/a
-  AVG(pt.offensive_rebounds),--Offensive Rebound h/a
-  AVG(pt.defensive_rebounds),--Defensive Rebound h/a
-  AVG(NULLIF(pt.field_goals_percentage,0)),--Field Goal Percentage % h/a
-  AVG(NULLIF(pt.three_point_field_goals_percentage,0)),--3-Point Field Goal % h/a
-  AVG(NULLIF(pt.free_throws_percentage,0)),--Free throw % h/a
-  AVG(pt.turnover),--Turnover h/a
-  AVG(pt.assists)--Assists h/a AVG 
-	from match_data as md INNER JOIN participation as pt 
-    ON md.fk_participation_home = pt.participation_id 
-    WHERE pt.fk_team_id = 2 
-    and pt.team_is_home = 1
-    and md.date > '2000-01-08'
-    and md.date < '2000-02-20'
-    order by md.date ASC;   
-"""
-
-# EXPLICAÇÃO DA USABILIDADE DO SELECT ACIMA
-# temporada começou em 08/01/2000
-# O ag ta no dia 20/02/2000
-# lembrando q estamos ignorando SPREAD e a DIFICULDADE ENFRENTADA por enquanto
 
 
 def get_match_amount():
@@ -125,17 +84,17 @@ def get_match_amount():
 
 
 def get_matches_by_season(date):
-    """Escolhe uma partida aleatória entre todas as partidas salvas e retorna alguns dados
-    referentes a ela.
+    """Escolhe todas as partidas da season -exceto dos 10 primeiros dias- e retorna alguns dados
+    referentes a elas.
 
     Args:
-        match_amount (int): Quantidade de partidas salvas para escolher um índice aleatoriamente
+        date (int): ano de inicio de uma temporada.
 
     Raises:
         e: Exceção do tipo Exception
 
     Returns:
-        list: Uma lista com os seguintes valores da partida aleatória:
+        list: Uma lista com partidas e para cada partida uma lista com os seguintes valores:
             int dizendo se o time em casa ganhou (1=true, 0=false)
             id correspondendo à fk do time em casa
             id correspondente à fk do time fora
@@ -145,6 +104,12 @@ def get_matches_by_season(date):
     # pegar match aleatoria a partir de id
     season_start = get_start_season_by_date(date)
     #season_end   = seasons[year_date]["end"]
+    season_start_list = season_start.split('-')
+
+
+
+    str_for_sql_date_start = str(dt.strptime(season_start, "%Y-%m-%d").date() + timedelta(days=10)) 
+    str_for_sql_date_end = str(date[0])+"-"+str(date[1])+"-"+str(date[2])
 
     try:
         db_connection = sqlite3.connect(join(Directory(Path(__file__).resolve().parent.parent.parent).cwd,
@@ -159,7 +124,7 @@ def get_matches_by_season(date):
                     WHERE md.date >= ?
                     and   md.date <= ?
                     order by md.date desc
-            """, [season_start, str(date[0])+"-"+str(date[1])+"-"+str(date[2])])
+            """, [season_start, str_for_sql_date_end])
 
         lista = cursor.fetchall()
 
